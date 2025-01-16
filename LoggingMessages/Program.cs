658930a6a745e60deab45c0de1ec0adb5c2d7724
KolesovDI <kolesovdi@it.mos.ru>
2025-01-16 22:52:58 +0300

    Init

---
+++ LoggingMessages/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggingMessages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ OrderProcessing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
