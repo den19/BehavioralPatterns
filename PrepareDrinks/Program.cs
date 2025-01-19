0255994486126edcd7f4d5f47199e7fae114a6d9
KolesovDI <kolesovdi@it.mos.ru>
2025-01-19 09:03:06 +0300

    Template Method. Prepare Drinks.

---
+++ PrepareDrinks/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.NetworkInformation;
+using System.Runtime.ConstrainedExecution;
+
+namespace PrepareDrinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage coffee = new Coffee();
+            coffee.PrepareRecipe();
+
+            Console.WriteLine("\n\n");
+
+            Beverage tea = new Tea();
+            tea.PrepareRecipe();
+        }
+    }
+
+    abstract class Beverage // Напиток
+    {
+        // Шаблонный метод
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiment();
+        }
+
+        // Шаги алгоритма
+        protected abstract void Brew(); // Заваривать
+        protected abstract void AddCondiment(); // Приправа
+        protected void BoilWater()
+        {
+            Console.WriteLine("Кипячение воды");
+        }
+        protected void PourInCup()
+        {
+            Console.WriteLine("Разлив напитка по чашкам");
+        }
+    }
+
+    class Coffee : Beverage
+    {
+        protected override void AddCondiment()
+        {
+            Console.WriteLine($"Добавление сахара и молока");
+        }
+
+        protected override void Brew()
+        {
+            Console.WriteLine("Заваривание кофе");
+        }
+    }
+
+    class Tea : Beverage
+    {
+        protected override void AddCondiment()
+        {
+            Console.WriteLine($"Добавление лимона");
+        }
+
+        protected override void Brew()
+        {
+            Console.WriteLine("Заваривание чая");
+        }
+    }
+}
