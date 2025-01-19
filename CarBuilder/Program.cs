5cec6d9ef42d0971689c701a1ed4498184664ba9
KolesovDI <kolesovdi@it.mos.ru>
2025-01-19 20:37:28 +0300

    Template Method. Car Builder.

---
+++ CarBuilder/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace CarBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarBuilder sedanBuilder = new SedanBuilder();
+            sedanBuilder.BuildCar();
+
+            Console.WriteLine("\n\n");
+
+            CarBuilder suvBuilder = new SUVBuilder();
+            suvBuilder.BuildCar();
+        }
+    }
+
+    abstract class CarBuilder
+    {
+        // Шаблонный метод
+        public void BuildCar()
+        {
+            InstallEngine();
+            InstallWheels();
+            PaintCar();
+        }
+
+        // Шаги алгоритма
+        protected abstract void InstallEngine();
+        protected abstract void InstallWheels();
+        protected abstract void PaintCar();
+    }
+
+    class SedanBuilder : CarBuilder
+    {
+        protected override void InstallEngine()
+        {
+            Console.WriteLine("Install sedan engine");
+        }
+
+        protected override void InstallWheels()
+        {
+            Console.WriteLine("Install sedan engine");
+        }
+
+        protected override void PaintCar()
+        {
+            Console.WriteLine("Paint sedan");
+        }
+    }
+
+    class SUVBuilder : CarBuilder
+    {
+        protected override void InstallEngine()
+        {
+            Console.WriteLine("Install SUV engine");
+        }
+
+        protected override void InstallWheels()
+        {
+            Console.WriteLine("Install SUV wheels");
+        }
+
+        protected override void PaintCar()
+        {
+            Console.WriteLine("Paint SUV");
+        }
+    }
+}
