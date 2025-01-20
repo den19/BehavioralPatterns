51d697765ab6a471aab3053c44831a746dbf644d
KolesovDI <kolesovdi@it.mos.ru>
2025-01-20 16:13:37 +0300

    Visitor. Simplification. Dynamic Dispatching.

---
+++ DynamicDispatching/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DynamicDispatching
+{
+    /*
+    C# поддерживает динамическую диспетчеризацию, которая позволяет автоматически выбирать нужный метод
+    на этапе выполнения программы. Это уменьшает необходимость в явной реализации методов для каждого типа элемента.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface IVisitor
+    {
+        void Visit(dynamic element);
+    }
+
+    public abstract class Element
+    {
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+    public class ConcreteElement : Element
+    {
+    }
+
+    public class ConcreteVisitor : IVisitor
+    {
+        public void Visit(dynamic element)
+        {
+            // Реализация логики посещения
+        }
+    }
+
+
+}
