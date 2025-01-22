63789277f0be97e6233cdc7711b644ff5bf71889
KolesovDI <kolesovdi@it.mos.ru>
2025-01-22 06:03:52 +0300

    Iterator

---
+++ SimpleListRealisation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleListRealisation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    // Интерфейс итератора
+    interface IIterator
+    {
+        bool HasNext();
+        object Current();
+        void Next();
+    }
+
+    // Агрегат (коллекция)
+    class MyList
+    {
+        private List<string> items = new List<string>();
+    }
+}
