07e5c07afbba095c67b5465484b04d8d623818cf
KolesovDI <den.kolesov@gmail.com>
2025-02-02 21:13:46 +0300

    Command. Task System.

---
+++ TaskSystem/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace TaskSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskList = new TaskList();
+
+            var task1 = new Task("Задача 1");
+            var task2 = new Task("Задача 2");
+
+            taskList.AddTask(task1);
+            taskList.AddTask(task2);
+
+            taskList.ExecuteAllTasks();
+
+            taskList.UnexecuteAllTasks();
+        }
+    }
+
+    public interface ITask
+    {
+        void Execute();
+        void Unexecute();
+    }
+
+    public class Task : ITask
+    {
+        private readonly string _taskDescription;
+        private bool _executed;
+
+        public Task(string taskDescription)
+        {
+            _taskDescription = taskDescription;
+        }
+
+        public void Execute()
+        {
+            _executed = true;
+            Console.WriteLine($"Задача \"{_taskDescription}\" выполнена.");
+        }
+
+        public void Unexecute()
+        {
+            _executed = false;
+            Console.WriteLine($"Задача \"{_taskDescription}\" отменена.");
+        }
+    }
+
+    public class TaskList
+    {
+        private List<ITask> _tasks = new List<ITask>();
+
+        public void AddTask(ITask task)
+        {
+            _tasks.Add(task);
+        }
+
+        public void RemoveTask(ITask task)
+        {
+            _tasks.Remove(task);
+        }
+
+        public void ExecuteAllTasks()
+        {
+            foreach (var task in _tasks)
+            {
+                task.Execute();
+            }
+        }
+
+        public void UnexecuteAllTasks()
+        {
+            foreach (var task in _tasks)
+            {
+                task.Unexecute();
+            }
+        }
+    }
+}
