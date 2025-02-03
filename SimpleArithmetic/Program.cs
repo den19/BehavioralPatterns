05dd3a8c911e8879002174c616e820fb4bc8f6c4
Denis Kolesov <den.kolesov@gmail.com>
2025-02-03 13:26:29 +0300

    Interpreter. Simple Arithmetic.

--- SimpleArithmetic/Program.cs
+++ SimpleArithmetic/Program.cs
@@ -5,17 +5,17 @@
         static void Main(string[] args)
         {
             // Создаем контекст
-            Context context = new Context("10+20");
+            Context context = new Context("10+(20-30)");
 
             // Создаем выражение
             Expression expression = new AdditionExpression(
                 new NumberExpression(10),
-                new NumberExpression(20));
+                new SubtractionExpression(new NumberExpression(20), new NumberExpression(30)));
 
             // Интерпретируем выражение
             int result = expression.Interpret(context);
 
-            Console.WriteLine(result); // Выведет 30
+            Console.WriteLine(result); // Выведет 0
 
         }
     }
@@ -69,4 +69,23 @@
             return _left.Interpret(context) + _right.Interpret(context);
         }
     }
+
+    // Конкретная реализация для вычитания
+    class SubtractionExpression : Expression
+    {
+        private Expression _left;
+        private Expression _right;
+
+        public SubtractionExpression(Expression left, Expression right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override int Interpret(Context context)
+        {
+            return _left.Interpret(context) - _right.Interpret(context);
+        }
+    }
+
 }
