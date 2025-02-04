ea18e90dd9a7198bb8cfd4ae3ecf6e5d94746f60
KolesovDI <den.kolesov@gmail.com>
2025-02-04 06:01:17 +0300

    Memento. TextEditorUndo.

---
+++ TextEditorUndo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace TextEditorUndo
+{
+    /*
+    Поведенческий шаблон проектирования Хранитель (Memento) предназначен для сохранения состояния объекта
+    таким образом, чтобы оно могло быть восстановлено позже, не нарушая инкапсуляцию.
+    Это полезно, когда нужно сохранять состояние объекта, а затем возвращаться к нему,
+    но при этом не раскрывать внутреннее устройство самого объекта.
+    Основные компоненты шаблона Memento:
+    1.	Originator – класс, состояние которого необходимо сохранить.
+    2.	Caretaker – класс, который отвечает за сохранение и восстановление состояний Originator.
+    3.	Memento – объект, содержащий сохраненное состояние Originator. Важно, что сам Memento предоставляет ограниченный доступ к своему содержимому, обеспечивая тем самым инкапсуляцию.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    // Класс Originator
+    class Editor
+    {
+        private StringBuilder text;
+
+        public Editor(StringBuilder text)
+        {
+            this.text = text;
+        }
+
+        public void SetText(StringBuilder text)
+        {
+            this.text = text;
+        }
+
+        public StringBuilder GetText()
+        {
+            return text;
+        }
+
+        // Метод для создания хранителя (memento)
+        public EditorMemento CreateMemento()
+        {
+            return new EditorMemento(text.ToString());
+        }
+
+        // Метод для восстановления состояния из хранителя
+        public void RestoreFromMemento(EditorMemento memento)
+        {
+            text.Clear();
+            text.Append(memento.GetState());
+        }
+    }
+
+    // Класс Memento
+    class EditorMemento
+    {
+        private readonly string state;
+
+        public EditorMemento(string state)
+        {
+            this.state = state;
+        }
+
+        public string GetState()
+        {
+            return state;
+        }
+    }
+
+}
