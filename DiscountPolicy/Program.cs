3d8d7a67568c54e4ce0e6880f8f303884b1b0cb7
KolesovDI <den.kolesov@gmail.com>
2025-02-13 05:53:04 +0300

    Simple Policy. Discount Policy.

---
+++ DiscountPolicy/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace DiscountPolicy
+{
+    /*
+    Политика скидок для покупателей. 
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ISalePolicy policy = new BulkPurchaseSalePolicy();
+            ShoppingCart cart = new ShoppingCart(policy);
+
+            double price = 100;
+            int count = 15;
+            double discountedPrice = cart.ApplyDiscount(price, count);
+
+            Console.WriteLine($"Price after apply discount: {discountedPrice}");
+        }
+    }
+
+    public interface ISalePolicy
+    {
+        double CalculateDiscount(double price, int purchaseCount);
+    }
+
+    public class BulkPurchaseSalePolicy : ISalePolicy
+    {
+        public double CalculateDiscount(double price, int purchaseCount)
+        {
+            if (purchaseCount > 10)
+            {
+                return price * 0.9;
+            }
+
+            return price;
+        }
+    }
+
+    public class ShoppingCart
+    {
+        private readonly ISalePolicy _salePolicy;
+
+        public ShoppingCart(ISalePolicy salePolicy)
+        {
+            _salePolicy = salePolicy;
+        }
+
+        public double ApplyDiscount(double price, int count)
+        {
+            return _salePolicy.CalculateDiscount(price, count);
+        }
+    }
+}
